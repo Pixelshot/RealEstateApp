c233a5e369bc4800502a8c8d355da6576a56d119
pixelshot <pixelshot8@gmail.com>
2024-12-30 17:04:23 +0800

    Fonts and Assets

---
+++ app/properties/[id].tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+import {useLocalSearchParams} from "expo-router";
+
+const Property = () => {
+    const {id} = useLocalSearchParams(); // automatically set up id search using react native's way
+    return (
+        <View>
+            <Text>[Id</Text>
+        </View>
+    )
+}
+    export default Property;
